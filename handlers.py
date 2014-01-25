7e5a4590aa319153c7a5fb3285c616d64f6cc92d
Hans Engel <engel@engel.uk.to>
2014-01-24 23:59:50 -0800

    Add scaffolding

--- handlers.py
+++ handlers.py
@@ -1,10 +1,11 @@
 import json
-import logging
 from google.appengine.ext import webapp
 
 class BaseHandler(webapp.RequestHandler):
     """ Base RequestHandler to be inherited by all other
         RequestHandlers. """
+
+    JINJA_ENV = None
 
     def out(self, data):
         """ Write data to client  """
@@ -14,3 +15,26 @@ class BaseHandler(webapp.RequestHandler):
         """ Write json data to client """
         self.response.headers['Content-Type'] = 'application/json'
         self.out(json.dumps, data)
+
+    def out_template(self, filename, data=None):
+        """ Render and write a template to client.
+            `data` must be a dict if values need to be
+            sent to the template """
+
+        self.response.headers['Content-Type'] = 'text/html'
+        if data is None:
+            data = {}
+
+        # Add other values
+        # ---
+        data.update({
+            'APP_VERSION': os.environ['CURRENT_VERSION_ID'],
+            'HOST': self.request.host,
+            'PAGE_URL_FULL': self.request.path_url,
+            'QUERY_STRING': self.request.query_string,
+            'URL': self.request.url,
+            'PATH': self.request.path
+        })
+
+        template = self.JINJA_ENV.get_template(filename)
+        self.out(template.render(data))
