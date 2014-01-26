5bd36e0e4d2ebf26d570e5456ffd0f43d5d78734
Mitchell Douglass <mrdouglass95@gmail.com>
2014-01-25 20:55:42 -0800

    added some functionality for flashcards on site

--- static/script/index.js
+++ static/script/index.js
@@ -1,4 +1,18 @@
 $("document").ready(function(){
+	var cards = [
+					{ question: "First President of the United States",
+					    answer: "George Washington"
+					},
+					{
+					  question: "This man invented the lightbulb",
+					    answer: "Thomas Edison"
+					},
+					{
+					  question: "Do you love bacon?",
+					    answer: "YES!"
+					}
+				];
+	var cardIndex = 0;
 	$("#form").submit(function(){
 		$.ajax({
 			url: "/analyze/" + encodeURIComponent($("#term_in").val()) + ".json",
@@ -14,12 +28,16 @@ $("document").ready(function(){
 					$("#related_list").append("<a href=\"" + entry.url + "\"><div class=\"link\">" + entry.name + "</div></a>");
 				});
 
+				cards = data.cards;
+
 				if($("#terms_div").height() > $("#related_div").height()){
 					$("#related_div").css("height", $("#terms_div").height().toString());
 				} else if($("#terms_div").height() < $("#related_div").height()){
 					$("#terms_div").css("height", $("#related_div").height().toString());
 				}
 
+				$("card_div").css("height", $("card_div").width()*(3/4));
+				loadCard();
 				$("#terms_div").show();
 				$("#related_div").show();
 				$("#lower_div").show();
@@ -27,5 +45,33 @@ $("document").ready(function(){
 			}
 		});
 		return false;
+	});
+
+	function loadCard(){
+		$("#card_question").hide();
+		$("#card_answer").hide();
+		$("#card_question").empty();
+		$("#card_answer").empty();
+		$("#card_question").append(cards[cardIndex].question);
+		$("#card_answer").append(cards[cardIndex].answer);
+		$("#question").show();
+	}
+
+	$("#left_arrow").click( function(){
+		if(cardIndex == 0){
+			cardIndex = cards.length() - 1;
+		} else if (cardIndex > 0){
+			cardIndex--;
+		}
+		loadCard();
+	});
+
+	$("#right_arrow").click(function(){
+		if(cardIndex == cards.length() - 1){
+			cardIndex = 0;
+		} else if (cardIndex < cards.length() - 1){
+			cardIndex++;
+		}
+		loadCard();
 	});
 });
